b4f9d53a78a5d89b03752ab5a4e32a0c921a3d06
buysdb <i@buysdb.nl>
2019-06-09 22:53:44 +0200

    Fixed case sensitive name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 setup(
-	name='sht31array',
+	name='SHT31array',
 	version='0.01',
 	author='Buys de Barbanson',
 	author_email='code@buysdb.nl',
 	description='Sample from multiple sht31 sensors in real time',
 	url='https://github.com/BuysDB/SHT31-Sensor-Array',
-	py_modules=['SHT31_array'],
+	py_modules=['SHT31array'],
 	scripts=['SHT31_array_realtime.py'],
 	install_requires=['numpy']
 )
